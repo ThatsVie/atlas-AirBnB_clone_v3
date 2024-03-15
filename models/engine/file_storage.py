660577c2553ebf4a2d535b6192ca5f0473534c65
ThatsVie <viepaulalmt@gmail.com>
2024-03-15 12:04:10 -0500

    task3

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -82,6 +82,6 @@ class FileStorage:
     def count(self, cls=None):
         """
         Returns the number of objects in storage matching the given class.
-        If no class is passed, returns the count of all objects in storage.
+        If no class is passed,returns the count of all objects in storage.
         """
         return len(self.all(cls))
